321ca3cccfb2d527baf7625006c89fc561c307bf
jfelipe14 <juan_fquintero@soy.sena.edu.co>
2024-02-22 12:33:39 -0500

    ciclo for: ejemplo 1

---
+++ cicloFor/Uno.java
@@ -0,0 +1,10 @@
+// Escribir un algoritmo que muestre en la pantalla los múltiplos de 7 comprendidos entre 1 y 70 inclusive.
+package cicloFor;
+
+public class Uno {
+    public static void main(String[] args) {
+        for (int i = 7; i <= 70; i = i + 7) {
+            System.out.println(i);
+        }
+    }
+}
